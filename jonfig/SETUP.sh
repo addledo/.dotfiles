04480445064dedf162577a79ff2822aeea1e1fbf
addledo <jontyfpine@gmail.com>
2025-09-03 16:55:56 +0100

    Fix SETUP

--- jonfig/SETUP.sh
+++ jonfig/SETUP.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-mkdir -p ~/.config/ && cp -f ./tmux-sessionizer.conf $_
+mkdir -p ~/.config/tmux-sessionizer/ && cp -f ./tmux-sessionizer.conf $_
 mkdir -p ~/.local/scripts/ && cp -f ./tmux-sessionizer $_
