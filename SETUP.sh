0bead11d3b55d26be7cb2370e076c66720b454ec
addledo <jontyfpine@gmail.com>
2025-11-28 21:25:28 +0000

    Add copy-dotfiles.sh

--- SETUP.sh
+++ SETUP.sh
@@ -1,19 +1,13 @@
 #! /usr/bin/env bash
 
-#./install.sh
+echo "Copy dotfiles"
+./copy-dotfiles.sh
+
+echo "Install packages"
+./install.sh
 
 echo "Setup tmux sessionizer"
 pushd tmux > /dev/null
   ./tmux-setup.sh
 popd > /dev/null
- 
-dirs=(
-  wezterm
-  wofi
-  waybar
-  tmux
-)
 
-# cp -R "${dirs[@]}" ~/.config
-
-#mkdir -p ~/.config/wezterm/ && cp -f ./-sessionizer $_
