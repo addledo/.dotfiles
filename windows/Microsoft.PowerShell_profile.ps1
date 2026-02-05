8211ab4fb81d84d931df6068b8e1d15bf3d80d1e
Jonty Pine <150440645+addledo@users.noreply.github.com>
2026-02-05 10:04:40 +0000

    Update PowerShell profile

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,9 @@ Invoke-Expression (& { (zoxide init powershell | Out-String) })
 # Init starship. Requires installation first.
 Invoke-Expression (&starship init powershell)
 
+# Turn off autocomplete suggestions
+Set-PSReadLineOption -PredictionSource None
+
 # Wrapper for Yazi as provided by setup guide.
 function y {
 	$tmp = (New-TemporaryFile).FullName
@@ -14,31 +17,35 @@ function y {
 	Remove-Item -Path $tmp
 }
 
-# Make cd work like in linux
-del alias:cd -Force # Remove builtin cd alias to Set-Location
-function cd {
-	$pwd = Get-Location
-	# No args goes to home directory
-	if ($args.Count -eq 0) {
-		Set-Location ~
-	# '-' as arg goes to previous directory
-	} elseif ($args[0] -eq "-") {
-		Set-Location @global:OLDPWD
-	} else {
-		Set-Location @args
-	}
-	$global:OLDPWD = $pwd
-}
 
-# Aliases -----------------------
-# Neovim aliases
+# # Make cd work like in linux
+# Not needed for PowerShell 7
+# del alias:cd -Force # Remove builtin cd alias to Set-Location
+# function cd {
+# 	$pwd = Get-Location
+# 	# No args goes to home directory
+# 	if ($args.Count -eq 0) {
+# 		Set-Location ~
+# 	# '-' as arg goes to previous directory
+# 	} elseif ($args[0] -eq "-") {
+# 		Set-Location @global:OLDPWD
+# 	} else {
+# 		Set-Location @args
+# 	}
+# 	$global:OLDPWD = $pwd
+# }
+
 function vi { & nvim $args }
-# Git aliases
+
 function gs { & git status -- $args }
+del alias:gm -Force # Remove builtin gm alias to Get-Member
 function gm { & git commit -m $args }
 function gma { & git commit -am $args }
 function g. { & git add . }
 function gc { & git switch $args }
-function root { & cd (git rev-parse --show-toplevel) }
-# Edit powershell config
+function yeet { & git push }
+
 function prof { & nvim $profile }
+
+function sd {}
+function root { & cd (git rev-parse --show-toplevel) }
